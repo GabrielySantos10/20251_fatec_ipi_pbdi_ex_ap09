33f08d1f24d3f1a62c0657a426a3638beb6841e7
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-03-30 23:03:05 -0300

    feat: implementa o exercicio 1.1 utilizando if

---
+++ ap_09/script.sql
@@ -0,0 +1,22 @@
+-- 1.1 Faça um programa que exibe se um número inteiro é múltiplo de 3
+-- IF 
+DO
+$$
+DECLARE 
+    numero INT := valor_aleatorio_entre(1,100);
+BEGIN
+    IF numero % 3 = 0 THEN 
+        RAISE NOTICE '% é um número múltiplo de 3', numero;
+    ELSE
+        RAISE NOTICE '%  não é um número múltiplo de 3', numero;
+    END IF; 
+END 
+$$
+
+-- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+-- INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+-- END;
+-- $$ LANGUAGE plpgsql;
