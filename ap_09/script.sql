8f5bb79d7fcfddf28a75eccd3578d324b7e4067b
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-03-30 23:46:45 -0300

    feat: implementa o exercicio 1.2 utilizando case

--- ap_09/script.sql
+++ ap_09/script.sql
@@ -1,21 +1,41 @@
 -- 1.2 Faça um programa que exibe se um número inteiro é múltiplo de 3 ou de 5.
--- IF 
+-- CASE
+
 DO
 $$
 DECLARE 
     numero INT := valor_aleatorio_entre(1,100);
 BEGIN
-    IF numero % 3 = 0 and numero % 5 = 0 THEN 
-        RAISE NOTICE '% é um número múltiplo de 3 e de 5', numero;
-    ELSEIF numero % 3 = 0 THEN 
-        RAISE NOTICE '% é um número múltiplo de 3', numero;
-    ELSEIF numero % 5 = 0 THEN 
-        RAISE NOTICE '% é um número múltiplo de 5', numero;
-    ELSE
-        RAISE NOTICE '%  não é um número múltiplo de 3 e nem de 5', numero;
-    END IF; 
+    CASE 
+        WHEN numero % 3 = 0 and numero % 5 = 0 THEN 
+            RAISE NOTICE '% é um número múltiplo de 3 e de 5', numero;
+        WHEN numero % 3 = 0 THEN 
+            RAISE NOTICE '% é um número múltiplo de 3', numero;
+        WHEN numero % 5 = 0 THEN 
+            RAISE NOTICE '% é um número múltiplo de 5', numero;
+        ELSE
+            RAISE NOTICE '%  não é um número múltiplo de 3 e nem de 5', numero;
+    END CASE; 
 END 
 $$
+
+-- IF 
+-- DO
+-- $$
+-- DECLARE 
+--     numero INT := valor_aleatorio_entre(1,100);
+-- BEGIN
+--     IF numero % 3 = 0 and numero % 5 = 0 THEN 
+--         RAISE NOTICE '% é um número múltiplo de 3 e de 5', numero;
+--     ELSEIF numero % 3 = 0 THEN 
+--         RAISE NOTICE '% é um número múltiplo de 3', numero;
+--     ELSEIF numero % 5 = 0 THEN 
+--         RAISE NOTICE '% é um número múltiplo de 5', numero;
+--     ELSE
+--         RAISE NOTICE '%  não é um número múltiplo de 3 e nem de 5', numero;
+--     END IF; 
+-- END 
+-- $$
 
 -- 1.1 Faça um programa que exibe se um número inteiro é múltiplo de 3
 -- CASE 
