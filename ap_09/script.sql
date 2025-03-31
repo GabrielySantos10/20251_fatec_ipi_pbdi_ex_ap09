bbc3e432419045035a977846702f6df08fd45eb8
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-03-31 00:30:57 -0300

    feat: implementa o exercicio 1.3 utilizando if

--- ap_09/script.sql
+++ ap_09/script.sql
@@ -1,23 +1,58 @@
+--1.3 Faça um programa que opera de acordo com o seguinte menu.
+-- IF
+DO
+$$
+DECLARE
+    op1 INT := valor_aleatorio_entre(1,100);
+    op2 INT := valor_aleatorio_entre(1,100);
+    op INT := 4;
+    rest INT;
+BEGIN
+    RAISE NOTICE '
+    Qual operaçõe deseja executar?
+    Opções:
+     1 - Soma
+     2 - Subtração
+     3 - Multiplicação
+     4 - Divisão';
+    IF op = 1 THEN 
+        rest = op1 + op2;
+        RAISE NOTICE'% + % = %',op1,op2,rest;
+    ELSEIF op = 2 THEN 
+        rest = op1 - op2;
+        RAISE NOTICE'% - % = %',op1,op2,rest;
+    ELSEIF op = 3 THEN 
+        rest = op1 * op2;
+        RAISE NOTICE'% * % = %',op1,op2,rest;
+    ELSEIF op = 4 THEN 
+        rest = op1 / op2;
+        RAISE NOTICE'% / % = %',op1,op2,rest;
+    ELSE
+        RAISE NOTICE'Opção inválida';
+    END IF;
+END 
+$$
+
 -- 1.2 Faça um programa que exibe se um número inteiro é múltiplo de 3 ou de 5.
 -- CASE
 
-DO
-$$
-DECLARE 
-    numero INT := valor_aleatorio_entre(1,100);
-BEGIN
-    CASE 
-        WHEN numero % 3 = 0 and numero % 5 = 0 THEN 
-            RAISE NOTICE '% é um número múltiplo de 3 e de 5', numero;
-        WHEN numero % 3 = 0 THEN 
-            RAISE NOTICE '% é um número múltiplo de 3', numero;
-        WHEN numero % 5 = 0 THEN 
-            RAISE NOTICE '% é um número múltiplo de 5', numero;
-        ELSE
-            RAISE NOTICE '%  não é um número múltiplo de 3 e nem de 5', numero;
-    END CASE; 
-END 
-$$
+-- DO
+-- $$
+-- DECLARE 
+--     numero INT := valor_aleatorio_entre(1,100);
+-- BEGIN
+--     CASE 
+--         WHEN numero % 3 = 0 and numero % 5 = 0 THEN 
+--             RAISE NOTICE '% é um número múltiplo de 3 e de 5', numero;
+--         WHEN numero % 3 = 0 THEN 
+--             RAISE NOTICE '% é um número múltiplo de 3', numero;
+--         WHEN numero % 5 = 0 THEN 
+--             RAISE NOTICE '% é um número múltiplo de 5', numero;
+--         ELSE
+--             RAISE NOTICE '%  não é um número múltiplo de 3 e nem de 5', numero;
+--     END CASE; 
+-- END 
+-- $$
 
 -- IF 
 -- DO
