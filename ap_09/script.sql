972c23dea80ce89b2a4447fcc5a96a64eaf86552
Gabriely Santos <gabriely.s.santos085@gmail.com>
2025-03-31 22:32:54 -0300

    feat: implementa o exercicio 1.4 utilizando case

--- ap_09/script.sql
+++ ap_09/script.sql
@@ -1,24 +1,46 @@
 --  1.4 Um comerciante comprou um produto e quer vendê-lo com um lucro de 45% se o valor da compra for menor que R$20. Caso contrário, ele deseja lucro de 30%. Faça um programa que, dado o valor do produto, calcula o valor de venda.
--- IF 
+-- CASE
 DO $$
 DECLARE
     valor_compra INT := valor_aleatorio_entre(1,100);
     valor_venda FLOAT ;
     acrescimo_venda FLOAT ;
 BEGIN
-    IF valor_compra < 20 THEN
+    CASE
+        WHEN valor_compra <20 THEN
         acrescimo_venda := valor_compra * 0.45;
-		valor_venda := acrescimo_venda + valor_compra;
-    ELSE
-    	acrescimo_venda := valor_compra *0.30;
-		valor_venda := acrescimo_venda + valor_compra;
-    END IF;
-
-	RAISE NOTICE 
+        valor_venda := acrescimo_venda + valor_compra;
+        ELSE
+        acrescimo_venda := valor_compra * 0.30;
+        valor_venda := acrescimo_venda + valor_compra;
+    RAISE NOTICE 
     'o valor de compra foi: %, e com o acréscimo de: %, o valor de venda será de: %',
      valor_compra, acrescimo_venda, valor_venda;
+    END CASE;
 END;
 $$
+
+
+-- -- IF 
+-- DO $$
+-- DECLARE
+--     valor_compra INT := valor_aleatorio_entre(1,100);
+--     valor_venda FLOAT ;
+--     acrescimo_venda FLOAT ;
+-- BEGIN
+--     IF valor_compra < 20 THEN
+--         acrescimo_venda := valor_compra * 0.45;
+-- 		valor_venda := acrescimo_venda + valor_compra;
+--     ELSE
+--     	acrescimo_venda := valor_compra *0.30;
+-- 		valor_venda := acrescimo_venda + valor_compra;
+--     END IF;
+
+-- 	RAISE NOTICE 
+--     'o valor de compra foi: %, e com o acréscimo de: %, o valor de venda será de: %',
+--      valor_compra, acrescimo_venda, valor_venda;
+-- END;
+-- $$
 --1.3 Faça um programa que opera de acordo com o seguinte menu.
 -- -- CASE
 -- DO
